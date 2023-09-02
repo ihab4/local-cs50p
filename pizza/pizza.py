780f5759d42aa01c407494bb525b0c334eebae31
killua <killua@desktop>
2023-09-02 21:12:47 +0100

    correct with csv.reader

---
+++ pizza/pizza.py
@@ -0,0 +1,29 @@
+import csv
+from tabulate import tabulate
+import sys
+
+
+def main():
+    check_command_line()
+    try:
+        csvfile = open(sys.argv[1])
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+    rows = []
+    reader = csv.reader(csvfile)
+    for row in reader:
+        rows.append(row)
+
+    print(tabulate(rows, headers="firstrow", tablefmt="grid"))
+
+
+def check_command_line():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+
+main()
